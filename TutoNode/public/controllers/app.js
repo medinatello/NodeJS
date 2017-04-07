148b66d45858dee9252af7fa6b7f60af6b3096b8
medinatello <medinatello@gmail.com>
2017-04-07 16:15:44 -0300

    f

--- TutoNode/public/controllers/app.js
+++ TutoNode/public/controllers/app.js
@@ -1,1 +1,36 @@
 ﻿'use strict';
+angular.module('MainApp', [
+    'Authentication',
+    'ngRoute',
+    'ngCookies',
+    'ngMessages'
+])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/monedalist', {
+            controller: 'monedaListControler',
+            templateUrl: 'views/listmoneda.html'
+        })
+            .otherwise({ redirectTo: '/login' });
+    }])
+
+  .controller('monedaListControler', [
+    '$scope', '$http', '$location',
+    function ($scope, $http, $location) {
+        $http.get('/monedas').success(function (data) {
+            $scope.users = data;
+            console.log(data);
+
+        });
+        
+        
+        $scope.RecargarLista = function () {
+            console.log('Recargar la lista');
+            $http.get('/monedas').success(function (data) {
+                $scope.users = data;
+                console.log(data);
+            });
+
+        };
+    }
+])
