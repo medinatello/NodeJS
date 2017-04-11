0d223fa39ddd6b6516b64c8849d2a859258ff395
medinatello <medinatello@gmail.com>
2017-04-11 16:13:56 -0300

    a

--- TutoNode/public/controllers/app.js
+++ TutoNode/public/controllers/app.js
@@ -1,36 +1,54 @@
-﻿'use strict';
-angular.module('MainApp', [
-    'Authentication',
-    'ngRoute',
-    'ngCookies',
-    'ngMessages'
-])
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider
-            .when('/monedalist', {
-            controller: 'monedaListControler',
-            templateUrl: 'views/listmoneda.html'
-        })
-            .otherwise({ redirectTo: '/login' });
-    }])
+﻿//'use strict';
+//angular.module('MainApp', [
+//    'Authentication',
+//    'ngRoute',
+//    'ngCookies',
+//    'ngMessages'
+//])
+//    .config(['$routeProvider', function ($routeProvider) {
+//        $routeProvider
+//            .when('/monedalist', {
+//            controller: 'monedaListControler',
+//            templateUrl: '../views/listmoneda.html'
+//        })
+//            .otherwise({ redirectTo: '/login' });
+//    }])
 
-  .controller('monedaListControler', [
-    '$scope', '$http', '$location',
-    function ($scope, $http, $location) {
-        $http.get('/monedas').success(function (data) {
-            $scope.users = data;
-            console.log(data);
+//  .controller('monedaListControler', [
+//    '$scope', '$http', '$location',
+//    function ($scope, $http, $location) {
+//        $http.get('/moneda').success(function (data) {
+//            $scope.users = data;
+//            console.log(data);
 
-        });
+//        });
         
         
-        $scope.RecargarLista = function () {
-            console.log('Recargar la lista');
-            $http.get('/monedas').success(function (data) {
-                $scope.users = data;
-                console.log(data);
-            });
+//        $scope.RecargarLista = function () {
+//            console.log('Recargar la lista');
+//            $http.get('/moneda').success(function (data) {
+//                $scope.users = data;
+//                console.log(data);
+//            });
 
-        };
-    }
-])
+//        };
+//    }
+//])
+
+
+
+var scotchTodo = angular.module('scotchTodo', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+    
+    // when landing on the page, get all todos and show them
+    $http.get('/api/todos')
+		.success(function (data) {
+        $scope.todos = data;
+    })
+		.error(function (data) {
+        console.log('Error: ' + data);
+    });
+
+}
--- TutoNode/routes/moneda.js
+++ TutoNode/routes/moneda.js
@@ -3,12 +3,16 @@ var router = express.Router();
 var db = require('../models/moneda.js')
 
 
-/* GET users listing. */
-router.get('/', function (req, res) {
-    res.send('respond with a resource');
-});
+///* GET users listing. */
+//router.get('/', function (req, res) {
+//    res.send('respond with a resource');
+//});
 
 
-router.get('/All', db.getAllMonedas);
+//router.get('/All', db.getAllMonedas);
+
+
+router.get('/', db.getAllMonedas);
+
 
 module.exports = router;
